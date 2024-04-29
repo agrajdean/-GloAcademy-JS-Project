c420000937f805a2956b1d080bd0db6f401c232a
Andrei Grajdean <agrajdean@mail.ru>
2024-04-29 08:14:31 +0300

    выполнено второе задание

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-alert("Hello world!");
-console.log("Hello world!");
+let title = "My project";
+console.log(typeof title);
+let screens = "Простые, Сложные, Интерактивные";
+console.log(screens.length);
+let screenPrice = "49";
+console.log(
+  "Стоимость верстки экранов  " + screenPrice + " рублей/долларов/гривен/юани"
+);
+let rollback = "16";
+let fullPrice = "150000";
+console.log(
+  "“Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани"
+);
+console.log(typeof fullPrice);
+let adaptive = "true";
+console.log(typeof adaptive);
 
-const title = "a";
-const screens = "b";
-const screenPrice = "c";
-let rollback = 1;
-let fullPrice = 2;
-let adaptive = 4;
+screens = screens.toLowerCase(); //запись в нижнем регистре
+screens = screens.split(/\s*,\s*/); //разбиваем строку на массив
+console.log(screens);
+
+console.log(
+  "Процент отката посреднику за работу " +
+    fullPrice * (rollback / 100) +
+    " рублей"
+); //Процент отката посреднику за работу
